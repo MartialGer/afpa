250f66190468dd618959ff03b9790d8a3d149fb6
Velwitch <phil.pynson@gmail.com>
2023-04-18 11:00:03 +0200

    middleware agin

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,16 +8,16 @@ use App\Models\User;
 
 class RoleMiddleware
 {
-    public function handle($request, Closure $next)
-{
-    if (Auth::check()) {
-        $user = Auth::user();
-
-        if ($user->role_id == 1) {
-            return $next($request);
+    public function handle($request, Closure $next, ...$roleIds)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+    
+            if (in_array($user->role_id, $roleIds)) {
+                return $next($request);
+            }
         }
+    
+        return redirect('/');
     }
-
-    return redirect('/');
-}
 }
--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::prefix('/admin')->group(function () {
     Route::post('/meteo', [CtrlMeteo::class, 'selectVilleMeteo'])->name('meteo_post');
 
     //EVENEMENT
-    Route::resource('/evenements', 'App\Http\Controllers\EvenementController')->middleware('role');
+    Route::resource('/evenements', 'App\Http\Controllers\EvenementController')->middleware('role: 1,2');
 
     //AGENDA
     Route::get('/articles', [ArticleController::class, 'indexAdmin'])->name('articles.admin.index');
